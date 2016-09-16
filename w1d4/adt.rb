6c0aefd0f6ae8cb5cbd1119749234b7202d6a309
Vince Sangkhae <vsangk@macbook-pro.attlocal.net>
2016-09-16 00:40:49 -0700

    finish Map class

--- w1d4/adt.rb
+++ w1d4/adt.rb
@@ -35,3 +35,31 @@ class Queue
     @queue
   end
 end
+
+class Map
+  def initialize
+    @map = []
+    @current_keys = []
+  end
+
+  def assign(key, value)
+    @map << [key, value] unless @current_keys.include?(key)
+    @current_keys << key unless @current_keys.include?(key)
+
+    [key, value]
+  end
+
+  def lookup(key)
+    @map.each_index do |idx|
+      return @map[idx][1] if @map[idx][0] == key
+    end
+    nil
+  end
+
+  def remove(key)
+    pair = [key, lookup(key)]
+    @current_keys.delete(key)
+    @map.delete(pair)
+  end
+
+end
