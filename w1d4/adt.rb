3ed6312b1115f03ba0b7892bdabced585b84a5db
Vince Sangkhae <vsangk@macbook-pro.attlocal.net>
2016-09-15 23:41:25 -0700

    finish Stack class and Queue class

--- w1d4/adt.rb
+++ w1d4/adt.rb
@@ -1,17 +1,37 @@
 class Stack
   def initialize
-
+    @stack = []
   end
 
   def add(el)
-
+    @stack.push(el)
+    el
   end
 
   def remove
-
+    @stack.pop
   end
 
   def show
+    @stack
+  end
+end
 
+class Queue
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(el)
+    @queue.push(el)
+    el
+  end
+
+  def dequeue
+    @queue.shift
+  end
+
+  def show
+    @queue
   end
 end
