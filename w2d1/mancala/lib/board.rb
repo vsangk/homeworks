713d4e42d82df63f7e0a971562f3b72f7e1480fc
Vince Sangkhae <vsangk@macbook-pro.attlocal.net>
2016-09-19 23:27:11 -0700

    implement Board class

--- w2d1/mancala/lib/board.rb
+++ w2d1/mancala/lib/board.rb
@@ -2,20 +2,49 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @winning_cups = { name1 => 6, name2 => 13 }
+    @cups = self.place_stones
   end
 
   def place_stones
-    # helper method to #initialize every non-store cup with four stones each
+    # helper method to initialize every non-store cup with four stones each
+    cups = Array.new(14) { [] }
+    cups.each_with_index do |cup, idx|
+      next if @winning_cups.values.include?(idx)
+      4.times { cup << :stone }
+    end
   end
 
   def valid_move?(start_pos)
+    raise "Invalid starting cup" unless start_pos.between?(1, 14)
   end
 
   def make_move(start_pos, current_player_name)
+    stone_amt = @cups[start_pos]
+    @cups[start_pos] = []
+    cup_idx = start_pos
+
+    stone_amt.each_index do |idx|
+      cup_idx = (cup_idx + 1) % 14
+      if @winning_cups.values.include?(cup_idx) && cup_idx != @winning_cups[current_player_name]
+        cups_idx = (cup_idx + 1) % 14
+      end
+      @cups[cup_idx] << :stone
+    end
+
+    render
+    next_turn(cup_idx, current_player_name)
   end
 
-  def next_turn(ending_cup_idx)
+  def next_turn(ending_cup_idx, current_player_name)
     # helper method to determine what #make_move returns
+    if ending_cup_idx == @winning_cups[current_player_name]
+      :prompt
+    elsif @cups[ending_cup_idx].count > 1
+      ending_cup_idx
+    else
+      :switch
+    end
   end
 
   def render
@@ -27,8 +56,11 @@ class Board
   end
 
   def cups_empty?
+    @cups[0..5].all?(&:empty?) || @cups[7..12].all?(&:empty?)
   end
 
   def winner
+    return :draw if @cups[6] == @cups[13]
+    @cups[6].length > @cups[13].length ? @winning_cups.keys.first : @winning_cups.keys.last
   end
 end
